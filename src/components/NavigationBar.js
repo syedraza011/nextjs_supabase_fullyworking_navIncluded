dd3565ffa506399ca0aaab2588939a349e185dea
Syed Raza <syed.raza011@gmail.com>
2023-06-15 10:25:23 -0400

    update

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -99,7 +99,7 @@ function NavigationBar() {
               // href="/layout"
               // href="/middleware"
               // href="/head"
-              href="/SignIn"
+              href="/Home"
               className="rounded bg-blue-600 px-2 py-2 font-medium text-white transition duration-300 hover:bg-blue-800"
             >
               Log In
